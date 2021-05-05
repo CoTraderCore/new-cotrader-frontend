120db1f4a0319275f6d497df43983c76832ad10d
github2k20 <priyansh.44rastogi@gmail.com>
2021-05-05 15:29:21 +0530

    Updated implementation of wallet connect

--- src/components/ConnectWallets.js
+++ src/components/ConnectWallets.js
@@ -1,26 +1,75 @@
-import React, { Component } from 'react';
-import { observer } from "mobx-react"
+import React, { useEffect, useState } from "react";
+import { observer } from "mobx-react";
 
-export default observer(
-  
-  class Wallet extends Component {
-
-    connectWallet=async() =>{
-      await this.props.store.getweb3().then((response) => {
-        response.eth.getAccounts().then((result) => {console.log(result);this.props.store.accounts=result});
-      });
+const ConnectWallet = observer((props) => {
+  console.log(props)
+  const connectWallet = async () => {
+    try {
+      await props.store.getweb3();
+      props.store.accounts = await props.store.web3.eth.getAccounts();
+      
+    } catch (e) {
+      console.log(e);
     }
-    
-    render() { 
-      return (
-        <div>
-        {(this.props.store.account)? <h1>Connected</h1>: <a id="GFG" className="header-right wall" style={{"cursor":"pointer"}} onClick = {this.connectWallet}>Connect Wallet</a>}
-        </div>
-         
-        );
+  };
+
+  const disconnectWallet = async () => {
+    try {
+      props.store.web3Modal.clearCachedProvider();
+      props.store.account=false
+      props.store.web3Modal=null
+    } catch (e) {
+      console.log(e);
     }
-  }
-  
-);
+  };
 
+  useEffect(() => {
+    async function getBalance() {
+      if (props.store.accounts && props.store.web3) {
+        const balance = await props.store.web3.eth.getBalance(props.store.accounts[0]);
+        props.store.balance = parseFloat(
+          props.store.web3.utils.fromWei(balance.toString())
+        ).toFixed(4);
+      }
+    }
 
+    getBalance();
+  });
+
+  return (
+    <div>
+      {props.store.account ? (
+        <span>
+          {props.store.accounts ? (
+            <div>
+              <span>Address:{props.store.accounts[0]}</span>
+              <br />
+              <span>
+                Balance:{props.store.balance} ETH
+                <a
+                  className="header-right wall"
+                  style={{ cursor: "pointer" }}
+                  onClick={() => disconnectWallet()}
+                >
+                  Disconnect
+                </a>
+              </span>
+            </div>
+          ) : (
+            "Loading"
+          )}
+        </span>
+      ) : (
+        <a
+          className="header-right wall"
+          style={{ cursor: "pointer" }}
+          onClick={() => connectWallet()}
+        >
+          Connect Wallet
+        </a>
+      )}
+    </div>
+  );
+});
+
+export default ConnectWallet;
--- src/models/wallet_model.ts
+++ src/models/wallet_model.ts
@@ -2,21 +2,25 @@ import Web3 from "web3";
 import Web3Modal from "web3modal";
 import Authereum from "authereum";
 import Fortmatic from "fortmatic";
+import Portis from "@portis/web3";
 import WalletConnectProvider from "@walletconnect/web3-provider";
 import { makeObservable, observable } from "mobx";
 
 class WalletStore {
   loading: boolean = false;
   account: boolean = false;
-  web3: unknown;
-  accounts:unknown;
-
+  web3: any;
+  web3Modal:any;
+  accounts: any;
+  balance: any;
   constructor() {
     makeObservable(this, {
       account: observable,
       loading: observable,
       web3: observable,
-      accounts:observable,
+      accounts: observable,
+      balance: observable,
+      web3Modal:observable
     });
     // autorun(async () => this.web3 = await this.getweb3());
   }
@@ -24,18 +28,13 @@ class WalletStore {
   async getweb3() {
     this.loading = true;
     let providerOptions;
-    let web3Modal;
-    let web3;
+    // let web3Modal: Web3Modal;
+    let web3: unknown;
     providerOptions = {
-      authereum: {
-        package: Authereum, // required
-      },
-
       walletconnect: {
         package: WalletConnectProvider, // required
         options: {
-          infuraId:
-            "https://mainnet.infura.io/v3/36b6921562ec407e826fb24562a5c9db", // required
+          infuraId: "https://data-seed-prebsc-1-s1.binance.org:8545/", // required
         },
       },
 
@@ -45,42 +44,62 @@ class WalletStore {
           key: "pk_live_7E6A277E15DE415B", // required
         },
       },
+
+      portis: {
+        package: Portis, // required
+        options: {
+          id: "ae694217-1169-413a-95d4-9798f161a265", // required
+        },
+      },
     };
 
-    web3Modal = new Web3Modal({
+    this.web3Modal = new Web3Modal({
       network: "mainnet",
-      cacheProvider: true,
+      cacheProvider: false,
       disableInjectedProvider: false,
       providerOptions,
     });
 
-    const provider = await web3Modal.connect();
-    console.log(provider)
-    provider.on("accountsChanged", (accounts: string[]) => {
-      console.log("Account Disconnected");
-      this.account = false;
-      console.log(accounts);
-    });
-
-    // Subscribe to chainId change
-    provider.on("chainChanged", (chainId: number) => {
-      console.log(chainId);
-    });
-
-    // Subscribe to provider connection
-    provider.on("connect", (info: { chainId: number }) => {
-      console.log(info);
-    });
-
-    // Subscribe to provider disconnection
-    provider.on("disconnect", (error: { code: number; message: string }) => {
-      console.log(error);
-    });
-
+    const provider = await this.web3Modal.connect();
     web3 = new Web3(provider);
     this.loading = false;
     this.account = true;
     this.web3 = web3;
+
+    if (provider.on) {
+      provider.on("accountsChanged", (accounts: string[]) => {
+        console.log("Account Disconnected");
+        this.web3Modal.clearCachedProvider();
+        this.account = false;
+      });
+
+      // Subscribe to chainId change
+      provider.on("chainChanged", async (chainId: number) => {
+        if (this.web3) {
+          const balance = await this.web3.eth.getBalance(this.accounts[0]);
+          this.balance = parseFloat(
+            this.web3.utils.fromWei(balance.toString())
+          ).toFixed(4);
+          console.log(this.balance);
+        }
+
+        console.log(chainId);
+      });
+
+      // Subscribe to provider connection
+
+      provider.on("connect", (info: { chainId: number }) => {
+        console.log(info);
+      });
+
+      // Subscribe to provider disconnection
+      provider.on("disconnect", (error: { code: number; message: string }) => {
+        this.web3Modal.clearCachedProvider();
+
+        console.log(error);
+      });
+    }
+
     return web3;
   }
 }
